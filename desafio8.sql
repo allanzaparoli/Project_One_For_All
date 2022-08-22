807a7ed7a2e7a3057a0abd1e3018803b4571ca5f
allanzaparoli <allanzaparoli@gmail.com>
2022-08-21 22:30:43 -0300

    desafio8

---
+++ desafio8.sql
@@ -0,0 +1,5 @@
+SELECT art.nome AS artista, alb.album AS album
+FROM SpotifyClone.artista AS art
+INNER JOIN SpotifyClone.album AS alb
+ON art.artista_id = alb.artista_id
+HAVING artista = "Elis Regina";
